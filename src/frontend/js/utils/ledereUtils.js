037db43005114097feed75acf5294590f545f3e9
JMLindseth-nav <40055758+JMLindseth-nav@users.noreply.github.com>
2019-03-12 15:00:59 +0100

    Alltid vis møtebehovsiden uavhengig av om møtebehov er innsendt, Vis "ikke svart" for alle ledere i møtebehovperioden

--- src/frontend/js/utils/ledereUtils.js
+++ src/frontend/js/utils/ledereUtils.js
@@ -1,3 +1,12 @@
+import {
+    senesteTom,
+    tidligsteFom,
+} from './periodeUtils';
+import {
+    erOppfoelgingsdatoPassertMed16UkerOgIkke26Uker,
+    erOppfolgingstilfelleSluttDatoPassert,
+} from './motebehovUtils';
+
 export const filtrerLederePaaArbeidstakersMotebehov = (ledereData, motebehovData) => {
     return ledereData.filter((leder) => {
         return motebehovData.findIndex((motebehov) => {
@@ -14,7 +23,24 @@ export const fjernLedereMedInnsendtMotebehov = (ledereListe, motebehovData) => {
     });
 };
 
-export const finnLedereUtenInnsendtMotebehov = (ledereData, motebehovData) => {
-    const ledereFiltrertPaaArbeidstakersMotebehov = filtrerLederePaaArbeidstakersMotebehov(ledereData, motebehovData);
-    return fjernLedereMedInnsendtMotebehov(ledereFiltrertPaaArbeidstakersMotebehov, motebehovData);
+export const filtrerLederePaaOppfolgingstilfelleperioder = (ledereData, oppfolgingstilfelleperioder) => {
+    return ledereData.filter((leder) => {
+        const startOppfolgingsdato = oppfolgingstilfelleperioder[leder.orgnummer] ? tidligsteFom(oppfolgingstilfelleperioder[leder.orgnummer].data) : new Date();
+        const sluttOppfolgingsdato = oppfolgingstilfelleperioder[leder.orgnummer] ? senesteTom(oppfolgingstilfelleperioder[leder.orgnummer].data) : new Date();
+        const today = new Date();
+
+        startOppfolgingsdato.setHours(0, 0, 0, 0);
+        sluttOppfolgingsdato.setHours(0, 0, 0, 0);
+        today.setHours(0, 0, 0, 0);
+
+        return (startOppfolgingsdato && sluttOppfolgingsdato)
+            && !erOppfolgingstilfelleSluttDatoPassert(sluttOppfolgingsdato)
+            && erOppfoelgingsdatoPassertMed16UkerOgIkke26Uker(startOppfolgingsdato);
+    });
+};
+
+
+export const finnLedereUtenInnsendtMotebehov = (ledereData, motebehovData, oppfolgingstilfelleperioder) => {
+    const ledereFiltrertPaaOppfolgingstilfelleperioder = filtrerLederePaaOppfolgingstilfelleperioder(ledereData, oppfolgingstilfelleperioder);
+    return fjernLedereMedInnsendtMotebehov(ledereFiltrertPaaOppfolgingstilfelleperioder, motebehovData);
 };
