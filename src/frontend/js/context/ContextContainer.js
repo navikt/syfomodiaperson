74f99da73be1740ae18f06a319ea6104882935ca
Kristian Storvoll <kristian.storvoll@nav.no>
2019-07-31 11:46:16 +0200

    tc

--- src/frontend/js/context/ContextContainer.js
+++ src/frontend/js/context/ContextContainer.js
@@ -11,32 +11,27 @@ import {
 import { valgtEnhet } from '../actions/enhet_actions';
 import { hentVeilederinfo } from '../actions/veilederinfo_actions';
 import { opprettWebsocketConnection } from './contextHolder';
-import { post } from '../api';
 
 const opprettWSConnection = (actions, veilederinfo) => {
     const ident = veilederinfo.data.ident;
     opprettWebsocketConnection(ident, (wsCallback) => {
-        post('/sykefravaer/logs/info', {
-            message: 'wsCallback event',
-            data: wsCallback.data,
-        });
+        // eslint-disable-next-line no-console
+        console.log(wsCallback.data);
         if (wsCallback.data === CONTEXT_EVENT_TYPE.NY_AKTIV_BRUKER) {
-            actions.hentAktivEnhet({
+            actions.hentAktivBruker({
                 callback: (aktivBruker) => {
                     // eslint-disable-next-line no-undef
-                    if (aktivBruker !== fnr) {
-                        window.location = `/sykefravaer/${aktivBruker}`;
-                    }
+                    window.location.href = `/sykefravaer/${aktivBruker}`;
                 },
             });
         } else if (wsCallback.data === CONTEXT_EVENT_TYPE.NY_AKTIV_ENHET) {
             actions.hentAktivEnhet({
                 callback: (aktivEnhet) => {
-                    if (config.config.initiellEnhet !== aktivEnhet) {
-                        actions.valgtEnhet(aktivEnhet);
-                        config.config.initiellEnhet = aktivEnhet;
-                        window.renderDecoratorHead(config);
-                    }
+                    // eslint-disable-next-line no-console
+                    console.log(config);
+                    actions.valgtEnhet(aktivEnhet);
+                    config.config.initiellEnhet = aktivEnhet;
+                    window.renderDecoratorHead(config);
                 },
             });
         }
