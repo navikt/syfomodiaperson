03f3fc45253b67cbcb6e65a22f0a01047aa47179
Kristian Storvoll <kristian.storvoll@nav.no>
2019-08-01 14:18:22 +0200

    modal ved endring

--- src/frontend/js/context/ContextContainer.js
+++ src/frontend/js/context/ContextContainer.js
@@ -11,34 +11,89 @@ import {
 import { valgtEnhet } from '../actions/enhet_actions';
 import { hentVeilederinfo } from '../actions/veilederinfo_actions';
 import { opprettWebsocketConnection } from './contextHolder';
+import ModalWrapper, { } from 'nav-frontend-modal';
+import { Hovedknapp, Flatknapp } from 'nav-frontend-knapper';
 
-const opprettWSConnection = (actions, veilederinfo) => {
+
+const redirectNyttFnr = (fnr) => {
+    window.location.href = `/sykefravaer/${fnr}`;
+};
+
+const oppdaterAktivBruker = (nyttFnr) => {
+    redirectNyttFnr(nyttFnr);
+};
+
+const oppdaterAktivEnhet = (actions, nyEnhet) => {
     const config = require('../index').config;
+    actions.valgtEnhet(nyEnhet);
+    config.config.initiellEnhet = nyEnhet;
+    window.renderDecoratorHead(config);
+};
+
+const opprettWSConnection = (veilederinfo, wsCallback) => {
     const ident = veilederinfo.data.ident;
-    opprettWebsocketConnection(ident, (wsCallback) => {
-        if (wsCallback.data === CONTEXT_EVENT_TYPE.NY_AKTIV_BRUKER) {
-            actions.hentAktivBruker({
-                callback: (aktivBruker) => {
-                    if (aktivBruker !== config.config.fnr) {
-                        window.location.href = `/sykefravaer/${aktivBruker}`;
-                    }
-                },
-            });
-        } else if (wsCallback.data === CONTEXT_EVENT_TYPE.NY_AKTIV_ENHET) {
-            actions.hentAktivEnhet({
-                callback: (aktivEnhet) => {
-                    if (aktivEnhet !== config.config.initiellEnhet) {
-                        actions.valgtEnhet(aktivEnhet);
-                        config.config.initiellEnhet = aktivEnhet;
-                        window.renderDecoratorHead(config);
-                    }
-                },
-            });
-        }
-    });
+    opprettWebsocketConnection(ident, wsCallback);
+};
+
+const tekster = {
+    endretBrukerModal: {
+        header: 'Du har endret bruker',
+        beskrivelse: 'Du har endret bruker i et annet vindu. Du kan ikke jobbe med 2 brukere samtidig.\nVelger du å endre bruker mister du arbeidet du ikke har lagret.',
+        beholdKnapp: 'Behold gammel',
+        byttKnapp: 'Bytt til ny bruker',
+    },
+    endretEnhetModal: {
+        header: 'Du har endret enhet',
+        beskrivelse: 'Du har endret enhet i et annet vindu. Du kan ikke jobbe med 2 enheter samtidig.\nVelger du å endre enhet mister du arbeidet du ikke har lagret.',
+        beholdKnapp: 'Behold gammel',
+        byttKnapp: 'Bytt til ny enhet',
+    },
+};
+
+const endretSideModal = (visModal, endretType, nyttFnrEllerEnhet, byttTilNyClickHandler, beholdGammelClickHandler) => {
+    const modalTekster = endretType === 'bruker' ? tekster.endretBrukerModal : tekster.endretEnhetModal;
+    return (
+        <ModalWrapper
+            closeButton={false}
+            isOpen={visModal}>
+            <div>
+                <h2>{modalTekster.header}</h2>
+                <p>{modalTekster.beskrivelse}</p>
+                <p>Ønsker du å bytte til <strong>{nyttFnrEllerEnhet}</strong></p>
+                <div>
+                    <Hovedknapp
+                        onClick={() => {
+                            byttTilNyClickHandler();
+                        }}>
+                        {modalTekster.byttKnapp}
+                        </Hovedknapp>
+                    <Flatknapp
+                        onClick={() => {
+                            beholdGammelClickHandler();
+                        }}>
+                        {modalTekster.beholdKnapp}
+                    </Flatknapp>
+                </div>
+            </div>
+        </ModalWrapper>
+    );
 };
 
 export class Context extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visEndretBrukerModal: false,
+            visEndretEnhetModal: true,
+            nyttFnr: null,
+            nyEnhet: null,
+        };
+        this.onByttBrukerClicked = this.onByttBrukerClicked.bind(this);
+        this.onByttEnhetClicked = this.onByttEnhetClicked.bind(this);
+        this.skjulEndreModal = this.skjulEndreModal.bind(this);
+    }
+
     componentDidMount() {
         const {
             actions,
@@ -55,7 +110,64 @@ export class Context extends Component {
         } = this.props;
 
         if (!veilederinfo.hentet && nextProps.veilederinfo.hentet) {
-            opprettWSConnection(actions, nextProps.veilederinfo);
+            opprettWSConnection(nextProps.veilederinfo, (wsCallback) => {
+                if (wsCallback.data === CONTEXT_EVENT_TYPE.NY_AKTIV_BRUKER) {
+                    actions.hentAktivBruker({
+                        callback: (aktivBruker) => {
+                            this.visEndretBrukerModal(aktivBruker);
+                        },
+                    });
+                } else if (wsCallback.data === CONTEXT_EVENT_TYPE.NY_AKTIV_ENHET) {
+                    actions.hentAktivEnhet({
+                        callback: (aktivEnhet) => {
+                            this.visEndretEnhetModal(aktivEnhet);
+                        },
+                    });
+                }
+            });
+        }
+    }
+
+    onByttBrukerClicked() {
+        oppdaterAktivBruker(this.state.nyttFnr);
+    }
+
+    onByttEnhetClicked() {
+        oppdaterAktivEnhet(this.props.actions, this.state.nyEnhet);
+        this.skjulEndreModal();
+    }
+
+    skjulEndreModal() {
+        this.setState({
+            visEndretBrukerModal: false,
+            visEndretEnhetModal: false,
+            nyEnhet: null,
+            nyttFnr: null,
+        });
+    }
+
+    visEndretBrukerModal(nyttFnr) {
+        const config = require('../index').config;
+        const gammeltFnr = config.config.fnr;
+        if (nyttFnr && gammeltFnr !== nyttFnr) {
+            this.setState({
+                visEndretBrukerModal: true,
+                nyttFnr,
+                gammeltFnr,
+            });
+        }
+    }
+
+    visEndretEnhetModal(nyEnhet) {
+        const config = require('../index').config;
+        const gammelEnhet = config.config.initiellEnhet;
+        if (nyEnhet && gammelEnhet !== nyEnhet) {
+            this.setState({
+                visEndretBrukerModal: false,
+                visEndretEnhetModal: true,
+                nyEnhet,
+                gammelEnhet,
+            });
         }
     }
 
@@ -64,8 +176,16 @@ export class Context extends Component {
             veilederinfo,
         } = this.props;
 
+        const {
+            visEndretBrukerModal,
+            visEndretEnhetModal,
+            nyttFnr,
+            nyEnhet,
+        } = this.state;
         return (<div className="contextContainer">
-            { veilederinfo.hentingFeilet &&
+            {endretSideModal(visEndretBrukerModal, 'bruker', nyttFnr, this.onByttBrukerClicked, this.skjulEndreModal)}
+            {endretSideModal(visEndretEnhetModal, 'enhet', nyEnhet, this.onByttEnhetClicked, this.skjulEndreModal)}
+            {veilederinfo.hentingFeilet &&
                 <AlertStripe
                     className="contextContainer__alertstripe"
                     type="advarsel">
