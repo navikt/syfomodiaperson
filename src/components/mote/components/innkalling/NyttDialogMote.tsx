3e462acb544800b9e93c340479367753ed327c4c
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-10-21 14:10:16 +0200

    Add modal for choosing behandler.
    Foreløpig kun fastlege, venter på endepunkt i isdialogmote.
    Sender ikke valgt behandler til innkallingskjema, det kommer i neste runde.

--- src/components/mote/components/innkalling/NyttDialogMote.tsx
+++ src/components/mote/components/innkalling/NyttDialogMote.tsx
@@ -6,17 +6,62 @@ import { Link } from "react-router-dom";
 import { useDM2FeatureToggles } from "@/data/unleash/unleash_hooks";
 import { BehandlerParticipateModal } from "@/components/mote/components/innkalling/BehandlerParticipateModal";
 import { NyLosningModal } from "@/components/mote/components/innkalling/NyLosningModal";
+import { ChooseBehandlerModal } from "@/components/mote/components/innkalling/ChooseBehandlerModal";
+import { useSelector } from "react-redux";
+import { Fastlege } from "@/data/fastlege/types/Fastlege";
+import {
+  BehandlerDialogmeldingDTO,
+  BehandlerType,
+} from "@/data/behandlerdialogmelding/BehandlerDialogmeldingDTO";
+import { RootState } from "@/data/rootState";
 
 const texts = {
   nyttMote: "Nytt dialogmøte",
   nyttMoteTrackingContext: "Møtelandingsside: Opprett nytt dialogmøte",
 };
 
+const activeFastlege2BehandlerList = (fastlege?: Fastlege) => {
+  if (!fastlege) {
+    return [];
+  }
+
+  const behandler: BehandlerDialogmeldingDTO = {
+    type: BehandlerType.FASTLEGE,
+    fornavn: fastlege.fornavn || "",
+    mellomnavn: fastlege.mellomnavn,
+    etternavn: fastlege.etternavn || "",
+    fnr: fastlege.fnr || "",
+    partnerId: "",
+    herId: `${fastlege.herId}`,
+    hprId: fastlege.helsepersonellregisterId,
+    adresse: fastlege.fastlegekontor?.postadresse?.adresse,
+    orgnummer: fastlege.fastlegekontor?.orgnummer,
+    kontor: fastlege.fastlegekontor?.navn,
+    postnummer: fastlege.fastlegekontor?.postadresse?.postnummer,
+    poststed: fastlege.fastlegekontor?.postadresse?.poststed,
+    telefon: fastlege.fastlegekontor?.telefon,
+  };
+
+  return [behandler];
+};
+
 export const NyttDialogMote = (): ReactElement => {
   const [behandlerModalIsOpen, setBehandlerModalIsOpen] = useState(false);
+  const [chooseBehandlerModalIsOpen, setChooseBehandlerModalIsOpen] = useState(
+    false
+  );
   const [nyLosningModalIsOpen, setNyLosningModalIsOpen] = useState(false);
   const { brukerKanVarslesDigitalt } = useNavBrukerData();
-  const { isDm2FysiskBrevEnabled } = useDM2FeatureToggles();
+
+  // TODO Bytte ut denne når behandler api er på plass i isdialogmote (useBehandlereDialogmeldingQuery)
+  const fastlegeState = useSelector((state: RootState) => state.fastleger);
+  const activeFastlege = fastlegeState.aktiv;
+  const behandlere = activeFastlege2BehandlerList(activeFastlege);
+
+  const {
+    isDm2FysiskBrevEnabled,
+    isDm2InnkallingFastlegeEnabled,
+  } = useDM2FeatureToggles();
   const kanBrukeNyLosningInnkalling =
     brukerKanVarslesDigitalt || isDm2FysiskBrevEnabled;
 
@@ -53,7 +98,19 @@ export const NyttDialogMote = (): ReactElement => {
         isOpen={behandlerModalIsOpen}
         setIsOpen={setBehandlerModalIsOpen}
         setNyLosningModalIsOpen={setNyLosningModalIsOpen}
+        setChooseBehandlerModalIsOpen={setChooseBehandlerModalIsOpen}
+        isDm2InnkallingFastlegeEnabled={isDm2InnkallingFastlegeEnabled}
+        behandlere={behandlere}
       />
+
+      {isDm2InnkallingFastlegeEnabled && (
+        <ChooseBehandlerModal
+          isOpen={chooseBehandlerModalIsOpen}
+          setIsOpen={setChooseBehandlerModalIsOpen}
+          setNyLosningModalIsOpen={setNyLosningModalIsOpen}
+          behandlere={behandlere}
+        />
+      )}
 
       <NyLosningModal
         isOpen={nyLosningModalIsOpen}
