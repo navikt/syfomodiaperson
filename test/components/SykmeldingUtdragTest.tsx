754c88030b1ff5df2f1813341bec330401f053f6
Anders Rognstad <anders.rognstad@nav.no>
2022-01-07 08:07:49 +0100

    Rewrote test/components to tsx and react testing library
    Removed unused Brodsmuler code and mockdata

--- test/components/SykmeldingUtdragTest.tsx
+++ test/components/SykmeldingUtdragTest.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { expect } from "chai";
-import { mount } from "enzyme";
 import { SykmeldingUtdragContainer } from "@/components/speiling/sykepengsoknader/SykmeldingUtdragContainer";
-import mockSykepengesoknader from "../mockdata/mockSykepengesoknader";
 import mockSykmeldinger from "../mockdata/sykmeldinger/mockSykmeldinger";
 import SykmeldingUtdrag from "../../src/components/speiling/sykepengsoknader/soknad-felles/SykmeldingUtdrag";
 import { createStore } from "redux";
@@ -10,14 +8,32 @@ import { rootReducer } from "@/data/rootState";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import { newSMFormat2OldFormat } from "@/utils/sykmeldinger/sykmeldingParser";
+import { toDateWithoutNullCheck } from "@/utils/datoUtils";
+import {
+  SoknadstatusDTO,
+  SoknadstypeDTO,
+  SykepengesoknadDTO,
+} from "@/data/sykepengesoknad/types/SykepengesoknadDTO";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-const ARBEIDSTAKERSOKNAD_ID = "b9732cc7-6101-446e-a1ef-ec25a425b4fb";
 const realState = createStore(rootReducer).getState();
 const store = configureStore([]);
 const fnr = "12345000000";
-const soknad = mockSykepengesoknader.find(
-  (s) => s.id === ARBEIDSTAKERSOKNAD_ID
-);
+const soknad: SykepengesoknadDTO = {
+  arbeidsgiver: {
+    navn: "KONKURS BEDRIFT OG VENNER AS",
+    orgnummer: "000111222",
+  },
+  fom: toDateWithoutNullCheck("2018-08-18"),
+  id: "b9732cc7-6101-446e-a1ef-ec25a425b4fb",
+  opprettetDato: toDateWithoutNullCheck("2018-09-07"),
+  status: SoknadstatusDTO.NY,
+  sykmeldingId: "e425a750-7f39-4974-9a06-fa1775f987c9",
+  tom: toDateWithoutNullCheck("2018-08-31"),
+  soknadstype: SoknadstypeDTO.ARBEIDSTAKERE,
+  sporsmal: [],
+};
 const sykmelding = mockSykmeldinger.find((s) => {
   return s.id === soknad.sykmeldingId;
 });
@@ -25,7 +41,7 @@ const sykmelding = mockSykmeldinger.find((s) => {
 describe("SykmeldingUtdrag", () => {
   it("Skal hente sykmeldinger", () => {
     const mockStore = store({ ...realState });
-    mount(
+    render(
       <Provider store={mockStore}>
         <SykmeldingUtdragContainer fnr={fnr} soknad={soknad} />
       </Provider>
@@ -45,13 +61,15 @@ describe("SykmeldingUtdrag", () => {
       ...realState,
       sykmeldinger: { data: sykmeldingerData },
     });
-    const wrapper = mount(
+    const wrapper = render(
       <Provider store={mockStore}>
         <SykmeldingUtdragContainer fnr={fnr} soknad={soknad} />
       </Provider>
     );
-    expect(wrapper.find(SykmeldingUtdrag).prop("sykmelding").id).to.equal(
-      sykmelding.id
+    userEvent.click(wrapper.getByRole("button"));
+    expect(sykmelding?.sykmeldingStatus?.arbeidsgiver?.orgNavn).to.equal(
+      "PONTYPANDY FIRE SERVICE"
     );
+    expect(wrapper.getByText("PONTYPANDY FIRE SERVICE")).to.exist;
   });
 });
