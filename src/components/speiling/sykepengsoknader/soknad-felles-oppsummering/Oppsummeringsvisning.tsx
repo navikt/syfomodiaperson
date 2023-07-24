587a81ecde521924403b5c9c366056a2869215bd
Solgård <vetle.busol@outlook.com>
2023-07-24 14:55:37 +0200

    Fixed Oppsummeringsvisning

--- src/components/speiling/sykepengsoknader/soknad-felles-oppsummering/Oppsummeringsvisning.tsx
+++ src/components/speiling/sykepengsoknader/soknad-felles-oppsummering/Oppsummeringsvisning.tsx
@@ -4,6 +4,7 @@ import {
   SvarTypeDTO,
   SykepengesoknadDTO,
 } from "@/data/sykepengesoknad/types/SykepengesoknadDTO";
+import styled from "styled-components";
 
 export const getKey = (tag: string, id?: string | number): string =>
   `${tag}_${id}`;
@@ -16,24 +17,36 @@ const Oppsummeringsvisning: ({
   soknad: { sporsmal },
 }: OppsummeringsvisningProps) => ReactElement = ({
   soknad: { sporsmal },
-}: OppsummeringsvisningProps) => (
-  <>
-    {sporsmal
-      .filter(
-        (sporsmal) =>
-          sporsmal.svar.length > 0 ||
-          sporsmal.undersporsmal.length > 0 ||
-          sporsmal.svartype === SvarTypeDTO.IKKE_RELEVANT
-      )
-      .map((sporsmal) => (
-        <div
-          className="oppsummering__seksjon"
-          key={getKey(sporsmal.tag, sporsmal.id)}
-        >
-          <OppsummeringSporsmal {...sporsmal} />
-        </div>
-      ))}
-  </>
-);
+}: OppsummeringsvisningProps) => {
+  const SummarySection = styled.div`
+    border-bottom: 1px solid;
+    margin-bottom: 2em;
+    padding-bottom: 2em;
+    line-height: 1.3;
+
+    :last-child {
+      border-bottom-width: 0;
+      margin-bottom: 0;
+      padding-bottom: 0;
+    }
+  `;
+
+  return (
+    <>
+      {sporsmal
+        .filter(
+          (sporsmal) =>
+            sporsmal.svar.length > 0 ||
+            sporsmal.undersporsmal.length > 0 ||
+            sporsmal.svartype === SvarTypeDTO.IKKE_RELEVANT
+        )
+        .map((sporsmal) => (
+          <SummarySection key={getKey(sporsmal.tag, sporsmal.id)}>
+            <OppsummeringSporsmal {...sporsmal} />
+          </SummarySection>
+        ))}
+    </>
+  );
+};
 
 export default Oppsummeringsvisning;
