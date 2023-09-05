467f526861cf96368a23f160bb8ef0da04314e40
Solgård <vetle.busol@outlook.com>
2023-09-05 15:59:36 +0200

    IS-1572: Fix package.json

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,22 +1,22 @@
-import amplitude from "amplitude-js/amplitude.esm";
+import amplitude from "amplitude-js";
 import { erProd } from "@/utils/miljoUtil";
 
 /**
  See documentation for naming guidelines: https://github.com/navikt/analytics-taxonomy
  Other documentation on Aksel: https://aksel.nav.no/god-praksis/artikler/mal-brukeratferd-med-amplitude
  */
+enum EventType {
+  PageView = "besøk",
+}
+
 const getApiKey = () => {
   return erProd()
     ? "e4b68538f8d185f0ee2d913d8e51bd39"
     : "c7bcaaf5d0fddda592412234dd3da1ba";
 };
 
-export const logEvent = (eventType: string, eventProperties?: object) => {
-  client.logEvent(eventType, eventProperties);
-};
-
 export const logPageVisit = (url: string, sideTittel: string) => {
-  client.logEvent("besøk", { url: url, sidetittel: sideTittel });
+  client.logEvent(EventType.PageView, { url: url, sidetittel: sideTittel });
 };
 
 const client = amplitude.getInstance();
