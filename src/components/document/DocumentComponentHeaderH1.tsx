4f90041002da8a4f1d037c1d0a4834278ef401b8
Anders Rognstad <anders.rognstad@nav.no>
2023-11-22 12:16:56 +0100

    Replace flexrow with tailwind

--- src/components/document/DocumentComponentHeaderH1.tsx
+++ src/components/document/DocumentComponentHeaderH1.tsx
@@ -1,9 +1,8 @@
-import { FlexRow, PaddingSize } from "@/components/Layout";
 import { Heading } from "@navikt/ds-react";
 import React from "react";
 
 export const DocumentComponentHeaderH1 = ({ text }: { text: string }) => (
-  <FlexRow topPadding={PaddingSize.SM} bottomPadding={PaddingSize.SM}>
-    <Heading size="large">{text}</Heading>
-  </FlexRow>
+  <Heading className="py-4" size="large">
+    {text}
+  </Heading>
 );
