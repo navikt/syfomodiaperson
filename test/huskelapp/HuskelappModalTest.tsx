edaa77a7aa362876534d67b3f81019a3b0d42eb6
Solgård <vetle.bu.solgard@nav.no>
2023-11-23 14:45:01 +0100

    IS-1822: Fix tests

--- test/huskelapp/HuskelappModalTest.tsx
+++ test/huskelapp/HuskelappModalTest.tsx
@@ -4,26 +4,24 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { HuskelappModal } from "@/components/huskelapp/HuskelappModal";
 import { VEILEDER_IDENT_DEFAULT } from "../../mock/common/mockConstants";
-import {
-  HuskelappRequestDTO,
-  HuskelappResponseDTO,
-} from "@/data/huskelapp/huskelappTypes";
+import { HuskelappResponseDTO } from "@/data/huskelapp/huskelappTypes";
 import { generateUUID } from "@/utils/uuidUtils";
-import { changeTextInput } from "../testUtils";
 import { expect } from "chai";
 import userEvent from "@testing-library/user-event";
 import { stubHuskelappApi } from "../stubs/stubIshuskelapp";
 import nock from "nock";
 import { apiMock } from "../stubs/stubApi";
+import { Oppfolgingsgrunn } from "@/data/huskelapp/Oppfolgingsgrunn";
 
 let queryClient: QueryClient;
 let apiMockScope: any;
 
-const huskelappTekst = "Note to self";
+const huskelappOppfolgingsgrunn = Oppfolgingsgrunn.VURDER_DIALOGMOTE_SENERE;
+const huskelappOppfogingsgrunnText = "Vurder dialogmøte på et senere tidspunkt";
 const huskelapp: HuskelappResponseDTO = {
-  tekst: huskelappTekst,
   createdBy: VEILEDER_IDENT_DEFAULT,
   uuid: generateUUID(),
+  oppfolgingsgrunn: huskelappOppfolgingsgrunn,
 };
 
 const renderHuskelappModal = () =>
@@ -49,42 +47,11 @@ describe("HuskelappModal", () => {
     it("renders huskelapp-tekst with save, cancel and remove buttons", async () => {
       renderHuskelappModal();
 
-      const huskelappInput = (
-        await screen.findAllByRole("textbox", {
-          hidden: true,
-        })
-      )[0];
-      expect(huskelappInput.textContent).to.equal(huskelappTekst);
-      expect(screen.getByRole("button", { hidden: true, name: "Lagre" })).to
-        .exist;
+      expect(await screen.findByText(huskelappOppfogingsgrunnText)).to.exist;
       expect(screen.getByRole("button", { hidden: true, name: "Avbryt" })).to
         .exist;
       expect(screen.getByRole("button", { hidden: true, name: "Fjern" })).to
         .exist;
-    });
-    it("save posts huskelapp with expected text", async () => {
-      renderHuskelappModal();
-
-      const huskelappInput = (
-        await screen.findAllByRole("textbox", {
-          hidden: true,
-        })
-      )[0];
-      changeTextInput(huskelappInput, "New note to self");
-
-      const lagreButton = screen.getByRole("button", {
-        hidden: true,
-        name: "Lagre",
-      });
-      userEvent.click(lagreButton);
-
-      const lagreHuskelappMutation = queryClient.getMutationCache().getAll()[0];
-      const expectedHuskelapp: HuskelappRequestDTO = {
-        tekst: "New note to self",
-      };
-      expect(lagreHuskelappMutation.options.variables).to.deep.equal(
-        expectedHuskelapp
-      );
     });
     it("remove deletes huskelapp", async () => {
       renderHuskelappModal();
@@ -105,15 +72,10 @@ describe("HuskelappModal", () => {
     beforeEach(() => {
       stubHuskelappApi(apiMockScope, undefined);
     });
-    it("renders huskelapp input with save and cancel buttons", async () => {
+    it("renders huskelapp input with radio group and save and cancel buttons", async () => {
       renderHuskelappModal();
 
-      const huskelappInput = (
-        await screen.findAllByRole("textbox", {
-          hidden: true,
-        })
-      )[0];
-      expect(huskelappInput.textContent).to.be.empty;
+      expect(await screen.findByText("Velg oppfølgingsgrunn")).to.exist;
       expect(screen.getByRole("button", { hidden: true, name: "Lagre" })).to
         .exist;
       expect(screen.getByRole("button", { hidden: true, name: "Avbryt" })).to
