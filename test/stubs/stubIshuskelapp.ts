df3898810cc4b917867d2ce9bd81a2a728653901
Solgård <vetle.bu.solgard@nav.no>
2024-01-11 13:15:12 +0100

    IS-1970: Rename tests

--- test/stubs/stubIshuskelapp.ts
+++ test/stubs/stubIshuskelapp.ts
@@ -2,9 +2,11 @@ import nock from "nock";
 import { ISHUSKELAPP_ROOT } from "@/apiConstants";
 import { OppfolgingsoppgaveResponseDTO } from "@/data/oppfolgingsoppgave/types";
 
-export const stubHuskelappApi = (
+export const stubOppfolgingsoppgaveApi = (
   scope: nock.Scope,
-  huskelapp: OppfolgingsoppgaveResponseDTO | undefined
+  oppfolgingsoppgave: OppfolgingsoppgaveResponseDTO | undefined
 ) => {
-  scope.get(`${ISHUSKELAPP_ROOT}/huskelapp`).reply(200, () => huskelapp);
+  scope
+    .get(`${ISHUSKELAPP_ROOT}/huskelapp`)
+    .reply(200, () => oppfolgingsoppgave);
 };
