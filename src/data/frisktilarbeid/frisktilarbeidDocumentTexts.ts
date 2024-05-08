5d436a1606879fdeb106ef15defdd4b4ad8b81a6
Anders Rognstad <anders.rognstad@nav.no>
2024-05-08 12:34:23 +0200

    IS-2345: Fjern velg behandler og behandler document

--- src/data/frisktilarbeid/frisktilarbeidDocumentTexts.ts
+++ src/data/frisktilarbeid/frisktilarbeidDocumentTexts.ts
@@ -1,28 +1,9 @@
 import { tilLesbarDatoMedArUtenManedNavn } from "@/utils/datoUtils";
 
-export type BehandlermeldingTextsValues = {
-  fom: Date | undefined;
-  tom: Date | undefined;
-};
-
 export type VedtakTextsValues = {
   fom: Date | undefined;
   tom: Date | undefined;
 };
-
-export const getBehandlermeldingTexts = ({
-  fom,
-  tom,
-}: BehandlermeldingTextsValues) => ({
-  header: "Informasjon om vedtak om friskmelding til arbeidsformidling",
-  periode: `Periode fra ${toReadableDateOrEmpty(
-    fom
-  )} til ${toReadableDateOrEmpty(tom)}.`,
-  vedtak: "Din pasient har fått eget vedtak tilsendt.",
-  hjemmel: "Vedtaket er hjemlet i folketrygdloven § 8-5.",
-  noSykmelding:
-    "I perioden vedtaket gjelder skal du ikke skrive sykmelding til NAV, med mindre det skjer en forverring i pasientens helsesituasjon.",
-});
 
 export const getVedtakTexts = ({ fom, tom }: VedtakTextsValues) => ({
   header: "Vedtak om friskmelding til arbeidsformidling",
--- src/hooks/frisktilarbeid/useFriskmeldingTilArbeidsformidlingDocument.ts
+++ src/hooks/frisktilarbeid/useFriskmeldingTilArbeidsformidlingDocument.ts
@@ -6,8 +6,6 @@ import {
   createParagraphWithTitle,
 } from "@/utils/documentComponentUtils";
 import {
-  BehandlermeldingTextsValues,
-  getBehandlermeldingTexts,
   getVedtakTexts,
   VedtakTextsValues,
 } from "@/data/frisktilarbeid/frisktilarbeidDocumentTexts";
@@ -17,28 +15,9 @@ type VedtakDocumentValues = VedtakTextsValues & {
 };
 
 export const useFriskmeldingTilArbeidsformidlingDocument = (): {
-  getBehandlermeldingDocument(
-    values: BehandlermeldingTextsValues
-  ): DocumentComponentDto[];
   getVedtakDocument(values: VedtakDocumentValues): DocumentComponentDto[];
 } => {
-  const { getHilsen, getIntroGjelder, getBrukerNavnFnr } =
-    useDocumentComponents();
-
-  const getBehandlermeldingDocument = (
-    values: BehandlermeldingTextsValues
-  ): DocumentComponentDto[] => {
-    const behandlermeldingTexts = getBehandlermeldingTexts(values);
-    return [
-      createHeaderH1(behandlermeldingTexts.header),
-      getIntroGjelder(),
-      createParagraph(behandlermeldingTexts.periode),
-      createParagraph(behandlermeldingTexts.vedtak),
-      createParagraph(behandlermeldingTexts.hjemmel),
-      createParagraph(behandlermeldingTexts.noSykmelding),
-      getHilsen(),
-    ];
-  };
+  const { getHilsen, getBrukerNavnFnr } = useDocumentComponents();
 
   const getVedtakDocument = (
     values: VedtakDocumentValues
@@ -93,7 +72,6 @@ export const useFriskmeldingTilArbeidsformidlingDocument = (): {
   };
 
   return {
-    getBehandlermeldingDocument,
     getVedtakDocument,
   };
 };
