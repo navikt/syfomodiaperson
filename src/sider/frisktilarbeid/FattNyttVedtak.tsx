a5379776813e099d8e670e54c6127369a0b6b245
Solgård <vetle.bu.solgard@nav.no>
2024-07-02 15:22:37 +0200

    IS-2501: Add/Edit guidance text to 8-5 page

--- src/sider/frisktilarbeid/FattNyttVedtak.tsx
+++ src/sider/frisktilarbeid/FattNyttVedtak.tsx
@@ -3,21 +3,22 @@ import { BodyShort, Box, Heading, List } from "@navikt/ds-react";
 import { FattVedtakSkjema } from "@/sider/frisktilarbeid/FattVedtakSkjema";
 
 const texts = {
-  forberedelser: {
-    title: "Forberedelser",
-    intro:
-      "Her er noen punkter som må være på plass før du fatter vedtak og ordningen starter:",
-    preparations: [
-      "Forsikre deg om at utbetaling av sykepenger er igangsatt",
-      "Er bruker frisk nok til å bytte arbeid?",
-      "Bruker må være informert om ordningen og medfølgende plikter",
-      "Har du fått bekreftelse på oppsigelse?",
-      "Har du fått bekreftelse på fritak fra oppsigelsestiden?",
-      "Du må fatte et § 14a-vedtak i Arena",
-    ],
-    disclaimer:
-      "Du trenger ikke å sjekke infotrygd når du fatter vedtak herfra.",
-  },
+  title: "Forberedelser",
+  intro:
+    "Her er noen punkter som må være på plass før du fatter vedtak og ordningen starter:",
+  preparations: [
+    "Forsikre deg om at utbetaling av sykepenger er igangsatt",
+    "Er bruker frisk nok til å bytte arbeid?",
+    "Bruker må være informert om ordningen og medfølgende plikter",
+    "Har du fått bekreftelse på oppsigelse?",
+    "Har du fått bekreftelse på unntak fra arbeidsplikt i oppsigelsesperioden",
+    "Du må fatte et § 14a-vedtak i Arena",
+  ],
+  infoTitle: "Tilleggsinformasjon",
+  infoBulletPoints: [
+    "Du trenger ikke å sjekke Infotrygd når du fatter vedtak i Modia.",
+    "Av juridiske grunner skal ikke behandler få tilsendt kopi av vedtaket.",
+  ],
   button: "Vurder vedtak",
 };
 
@@ -26,17 +27,22 @@ export const FattNyttVedtak = (): ReactElement => {
     <div className="flex flex-col [&>*]:mb-4">
       <Box background="surface-default" padding="6">
         <Heading level="3" size="small">
-          {texts.forberedelser.title}
+          {texts.title}
         </Heading>
-        <BodyShort size="small">{texts.forberedelser.intro}</BodyShort>
+        <BodyShort size="small">{texts.intro}</BodyShort>
         <List as="ul" size="small">
-          {texts.forberedelser.preparations.map((text, index) => (
+          {texts.preparations.map((text, index) => (
             <List.Item key={index}>{text}</List.Item>
           ))}
         </List>
-        <BodyShort className="mt-6" size="small">
-          {texts.forberedelser.disclaimer}
-        </BodyShort>
+        <Heading level="4" size="xsmall">
+          {texts.infoTitle}
+        </Heading>
+        <List as="ul" size="small">
+          {texts.infoBulletPoints.map((text, index) => (
+            <List.Item key={index}>{text}</List.Item>
+          ))}
+        </List>
       </Box>
       <FattVedtakSkjema />
     </div>
