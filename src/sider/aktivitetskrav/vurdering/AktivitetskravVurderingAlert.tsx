450181400f6dc32bee7b852a09d2f6e69cbf1719
Solgård <vetle.bu.solgard@nav.no>
2024-10-10 14:53:09 +0200

    IS: Remove styled component alert

--- src/sider/aktivitetskrav/vurdering/AktivitetskravVurderingAlert.tsx
+++ src/sider/aktivitetskrav/vurdering/AktivitetskravVurderingAlert.tsx
@@ -9,8 +9,7 @@ import {
 } from "@/utils/datoUtils";
 import React, { ReactElement } from "react";
 import { avventVurderingArsakTexts } from "@/data/aktivitetskrav/aktivitetskravTexts";
-import { AktivitetskravAlertstripe } from "@/sider/aktivitetskrav/AktivitetskravAlertstripe";
-import { BodyLong, BodyShort, Label } from "@navikt/ds-react";
+import { Alert, BodyLong, BodyShort, Label } from "@navikt/ds-react";
 
 const texts = {
   forhandsvarselInfoBody:
@@ -33,21 +32,21 @@ export const AktivitetskravVurderingAlert = ({
   switch (status) {
     case AktivitetskravStatus.FORHANDSVARSEL: {
       return isExpiredForhandsvarsel(vurdering.varsel?.svarfrist) ? (
-        <AktivitetskravAlertstripe variant="warning">
+        <Alert variant="warning" className="mb-4">
           <Label size="small">{texts.forhandsvarselWarningLabel}</Label>
           <BodyShort size="small">{`Det ble sendt ut et forhåndsvarsel ${vurderingDato}.`}</BodyShort>
           <BodyShort size="small">{texts.forhandsvarselWarningBody}</BodyShort>
-        </AktivitetskravAlertstripe>
+        </Alert>
       ) : (
-        <AktivitetskravAlertstripe variant="info">
+        <Alert variant="info" className="mb-4">
           <Label size="small">{`Forhåndsvarsel er sendt ${vurderingDato}`}</Label>
           <BodyShort size="small">{texts.forhandsvarselInfoBody}</BodyShort>
-        </AktivitetskravAlertstripe>
+        </Alert>
       );
     }
     case AktivitetskravStatus.AVVENT: {
       return (
-        <AktivitetskravAlertstripe variant="warning">
+        <Alert variant="warning" className="mb-4" contentMaxWidth={false}>
           <Label size="small">
             {frist ? `Avventer til ${tilDatoMedManedNavn(frist)}` : "Avventer"}
           </Label>
@@ -62,7 +61,7 @@ export const AktivitetskravVurderingAlert = ({
               );
             })}
           </ul>
-        </AktivitetskravAlertstripe>
+        </Alert>
       );
     }
     default: {
