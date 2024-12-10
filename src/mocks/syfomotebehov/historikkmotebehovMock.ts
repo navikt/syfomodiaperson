7bb4260ce38814ae3fabf29a86405059ea056eb0
Eirik Dahlen <dahleneirik@gmail.com>
2024-12-10 19:13:51 +0100

    IS-2019: Add historikk motebehov from /motebehov endpoint

--- src/mocks/syfomotebehov/historikkmotebehovMock.ts
+++ src/mocks/syfomotebehov/historikkmotebehovMock.ts
@@ -1,27 +1,27 @@
 export const historikkmotebehovMock = [
   {
     opprettetAv: null,
-    tekst: "Møtebehovet ble lest av Z990000",
-    tidspunkt: "2019-04-15T00:00:00",
+    tekst: "Møtebehovet ble behandlet av Z990000",
+    tidspunkt: "2024-04-15T00:00:00",
   },
   {
     opprettetAv: null,
-    tekst: "Møtebehovet ble lest av Z990000",
-    tidspunkt: "2019-03-11T00:00:00",
+    tekst: "Sam Samuel Jones har svart på møtebehov",
+    tidspunkt: "2024-03-11T00:00:00",
   },
   {
     opprettetAv: null,
-    tekst: "Møtebehovet ble lest av Z990000",
-    tidspunkt: "2019-02-29T00:00:00",
+    tekst: "Møtebehovet ble behandlet av Z990000",
+    tidspunkt: "2024-02-29T00:00:00",
   },
   {
     opprettetAv: null,
-    tekst: "Møtebehovet ble lest av Z990000",
-    tidspunkt: "2019-01-10T00:00:00",
+    tekst: "Møtebehovet ble behandlet av Z990000",
+    tidspunkt: "2024-01-10T00:00:00",
   },
   {
     opprettetAv: null,
-    tekst: "Møtebehovet ble lest av Z990000",
-    tidspunkt: "2019-01-09T00:00:00",
+    tekst: "Møtebehovet ble behandlet av Z990000",
+    tidspunkt: "2024-01-09T00:00:00",
   },
 ];
