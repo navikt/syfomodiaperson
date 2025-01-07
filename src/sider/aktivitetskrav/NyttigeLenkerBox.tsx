94b2b0c98e2a4568cba1baa07487a60fa6a41d94
Ingrid Gladheim <ingridgladheim@hotmail.com>
2025-01-07 10:14:38 +0100

    IS-2604: add rundskriv lenke på aktivitetskravsiden

---
+++ src/sider/aktivitetskrav/NyttigeLenkerBox.tsx
@@ -0,0 +1,26 @@
+import { Box, Heading } from "@navikt/ds-react";
+import React from "react";
+import { EksternLenke } from "@/components/EksternLenke";
+
+const texts = {
+  heading: "Nyttige lenker",
+  rundskrivLink: "Rundskriv til § 8-8 andre ledd",
+};
+
+const rundskrivUrl =
+  "https://lovdata.no/pro/#document/NAV/rundskriv/r08-00/KAPITTEL_3-8-3";
+
+export default function NyttigeLenkerBox() {
+  return (
+    <Box
+      background="surface-default"
+      padding="4"
+      className="flex flex-col gap-4 mb-2"
+    >
+      <Heading level="2" size="medium">
+        {texts.heading}
+      </Heading>
+      <EksternLenke href={rundskrivUrl}>{texts.rundskrivLink}</EksternLenke>
+    </Box>
+  );
+}
