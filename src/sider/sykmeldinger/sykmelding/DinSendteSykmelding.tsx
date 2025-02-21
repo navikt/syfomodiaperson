451b6111e6f7d4ea7d4c25de92a87a4c7446eff9
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2025-02-21 11:26:45 +0100

    IS-3102: Fjern accordion (#1646)

--- src/sider/sykmeldinger/sykmelding/DinSendteSykmelding.tsx
+++ src/sider/sykmeldinger/sykmelding/DinSendteSykmelding.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import { SykmeldingOldFormat } from "@/data/sykmelding/types/SykmeldingOldFormat";
 import { SykmeldingOpplysninger } from "@/sider/sykmeldinger/sykmelding/sykmeldingOpplysninger/SykmeldingOpplysninger";
 import SykmeldingStatuspanel from "../sykmeldingstatuspanel/SykmeldingStatuspanel";
-import { SpeilingEkspanderbartPanel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanel";
-import { SpeilingEkspanderbartPanelTittel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanelTittel";
-
-const texts = {
-  tittel: "Dine opplysinger",
-};
 
 interface Props {
   sykmelding: SykmeldingOldFormat;
@@ -17,17 +11,7 @@ export function DinSendteSykmelding({ sykmelding }: Props) {
   return (
     <div>
       <SykmeldingStatuspanel sykmelding={sykmelding} />
-      <SpeilingEkspanderbartPanel
-        defaultOpen
-        variant="lysebla"
-        tittel={
-          <SpeilingEkspanderbartPanelTittel icon="person">
-            {texts.tittel}
-          </SpeilingEkspanderbartPanelTittel>
-        }
-      >
-        <SykmeldingOpplysninger sykmelding={sykmelding} />
-      </SpeilingEkspanderbartPanel>
+      <SykmeldingOpplysninger sykmelding={sykmelding} />
     </div>
   );
 }
