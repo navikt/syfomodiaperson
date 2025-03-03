8f3633b1501c2e9f47e1106ad1a17c53645a852a
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-03-03 13:01:51 +0100

    IS-3110: Endret tekster og utseende i skjemaet for å skrive innstilling ifm. manglende medvirkning for å reflektere at man ikke lenger trenger å trykke manuelt på stoppknappen

--- src/components/Sidetopp.tsx
+++ src/components/Sidetopp.tsx
@@ -5,12 +5,19 @@ const SIDETOPP_ID = "sidetopp";
 
 interface Props {
   tittel: string;
+  className?: string;
 }
 
-const Sidetopp = ({ tittel }: Props) => {
+const Sidetopp = ({ tittel, className }: Props) => {
   return (
     <header>
-      <Heading spacing size="large" id={SIDETOPP_ID} level="1">
+      <Heading
+        spacing
+        size="large"
+        id={SIDETOPP_ID}
+        level="1"
+        className={className}
+      >
         {tittel}
       </Heading>
     </header>
