9203a98dae2e7a0fa61741a57b6fe61acce41868
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-03-10 11:43:06 +0100

    IS-3110: Endret tekst på knapp

--- src/sider/manglendemedvirkning/ManglendeMedvirkningButtons.tsx
+++ src/sider/manglendemedvirkning/ManglendeMedvirkningButtons.tsx
@@ -9,7 +9,7 @@ import {
 import React from "react";
 
 const texts = {
-  stans: "Skriv innstilling",
+  stans: "Skriv innstilling om stans",
   oppfylt: "Oppfylt",
   unntak: "Unntak",
   ikkeAktuell: "Ikke aktuell",
