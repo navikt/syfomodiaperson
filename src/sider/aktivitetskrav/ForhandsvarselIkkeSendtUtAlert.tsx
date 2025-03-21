8730e6a4626e246baa47f0b257a6baf7f3829b71
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-03-21 11:44:13 +0100

    IS-3179: Lagt til innhold i varselboks

--- src/sider/aktivitetskrav/ForhandsvarselIkkeSendtUtAlert.tsx
+++ src/sider/aktivitetskrav/ForhandsvarselIkkeSendtUtAlert.tsx
@@ -5,10 +5,14 @@ import {
   AktivitetskravStatus,
   AktivitetskravVurderingDTO,
 } from "@/data/aktivitetskrav/aktivitetskravTypes";
+import { EksternLenke } from "@/components/EksternLenke";
 
 const texts = {
-  header: "Teknisk feil påvirket forhåndsvarsler mellom 27. februar - 12. mars",
-  info: "Grunnet teknisk feil har ikke forhåndsvarsel i perioden 27. februar – 12. mars blitt varslet på riktig måte. I mange av sakene har bruker fått forlenget frist til 9. april 2025, og fristen i Modia er oppdatert tilsvarende. Dersom berørte brukere tar kontakt, skal det gis forlenget frist.",
+  header:
+    "Har du sendt ut forhåndsvarsel om aktivitetskrav i perioden 27. februar - 12. mars?",
+  info: "Grunnet teknisk feil har ikke forhåndsvarsel i denne perioden blitt varslet på riktig måte. I saker der brev ble sendt før 10. mars har bruker fått ny frist for tilbakemelding 9. april. Bruker er informert om dette på innloggede sider, og frist i Modia syfo er oppdatert. Brev sendt etter 10. mars har ikke fått forlenget frist. Brukere som ber om utsatt frist skal få det. Les mer ",
+  url: "https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-sykefravarsoppfolging-og-sykepenger/SitePages/Teknisk-feil-med-varslinger-i-perioden-27.02.2025-til-13.03.2025.aspx",
+  linkText: "her",
 };
 
 export default function ForhandsvarselIkkeSendtUtAlert() {
@@ -37,7 +41,10 @@ export default function ForhandsvarselIkkeSendtUtAlert() {
         contentMaxWidth={false}
       >
         <Label>{texts.header}</Label>
-        <BodyShort>{texts.info}</BodyShort>
+        <BodyShort>
+          {texts.info}
+          <EksternLenke href={texts.url}>{texts.linkText}</EksternLenke>
+        </BodyShort>
       </Alert>
     )
   );
