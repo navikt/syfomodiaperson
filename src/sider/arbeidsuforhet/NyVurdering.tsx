cb3b2cbe4b8b8a79428823b4145b106e32c25048
Solgård <vetle.bu.solgard@nav.no>
2025-08-19 15:35:21 +0200

    IS: Remove text for previous forhandsvarsel and vurdering

--- src/sider/arbeidsuforhet/NyVurdering.tsx
+++ src/sider/arbeidsuforhet/NyVurdering.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { Alert, BodyShort, Box, Button, Heading } from "@navikt/ds-react";
-import { useGetArbeidsuforhetVurderingerQuery } from "@/sider/arbeidsuforhet/hooks/arbeidsuforhetQueryHooks";
-import {
-  typeTexts,
-  VurderingResponseDTO,
-  VurderingType,
-} from "@/sider/arbeidsuforhet/data/arbeidsuforhetTypes";
-import { tilLesbarDatoMedArUtenManedNavn } from "@/utils/datoUtils";
+import { Alert, Box, Button, Heading } from "@navikt/ds-react";
+
 import { useNotification } from "@/context/notification/NotificationContext";
 
 const texts = {
@@ -15,29 +9,11 @@ const texts = {
   startNyVurderingButton: "Start ny vurdering",
 };
 
-const lastVurderingText = (vurderinger: VurderingResponseDTO[]) => {
-  const lastVurdering = vurderinger[0];
-  if (!lastVurdering) {
-    return "Ingen vurderinger har blitt gjort, trykk på 'Start ny vurdering' for å sende forhåndsvarsel";
-  }
-  const lastForhandsvarsel = vurderinger.find(
-    (vurdering) => vurdering.type === VurderingType.FORHANDSVARSEL
-  );
-  const lastVurderingType = typeTexts[lastVurdering.type].toLowerCase();
-
-  return `Forrige forhåndsvarsel på 8-4 ble sendt ut ${tilLesbarDatoMedArUtenManedNavn(
-    lastForhandsvarsel?.createdAt
-  )} og ${lastVurderingType} ${tilLesbarDatoMedArUtenManedNavn(
-    lastVurdering?.createdAt
-  )}`;
-};
-
 interface Props {
   handleClick: () => void;
 }
 
 export default function NyVurdering({ handleClick }: Props) {
-  const { data: vurderinger } = useGetArbeidsuforhetVurderingerQuery();
   const { notification } = useNotification();
 
   return (
@@ -51,9 +27,6 @@ export default function NyVurdering({ handleClick }: Props) {
         <Heading className="mb-4" level="2" size="medium">
           {texts.siste}
         </Heading>
-        <BodyShort className="mb-4">{`${lastVurderingText(
-          vurderinger
-        )}`}</BodyShort>
         <Button onClick={handleClick} variant="secondary">
           {texts.startNyVurderingButton}
         </Button>
