58d86c48ec6685df16442b42a9485113d7df7543
geir-waagboe <geir.arne.waagbo@nav.no>
2025-09-08 15:14:49 +0200

    IS-3419: Sende med personident for mulige oppfolgingsenheter

--- src/data/oppfolgingsenhet/useGetMuligeOppfolgingsenheter.tsx
+++ src/data/oppfolgingsenhet/useGetMuligeOppfolgingsenheter.tsx
@@ -2,26 +2,32 @@ import { SYFOBEHANDLENDEENHET_ROOT } from "@/apiConstants";
 import { useQuery } from "@tanstack/react-query";
 import { get } from "@/api/axios";
 import { useBehandlendeEnhetQuery } from "@/data/behandlendeenhet/behandlendeEnhetQueryHooks";
+import { useValgtPersonident } from "@/hooks/useValgtBruker";
 
 export const muligeOppfolgingsenhetQueryKeys = {
-  muligeOppfolgingsenheter: (enhetId?: string) => [
+  muligeOppfolgingsenheter: (enhetId?: string, personident?: string) => [
     "muligeOppfolgingsenheter",
     enhetId,
+    personident,
   ],
 };
 
 export function useGetMuligeOppfolgingsenheter() {
   const { data: behandlendeenhet } = useBehandlendeEnhetQuery();
+  const personident = useValgtPersonident();
   const enhetId =
     behandlendeenhet?.oppfolgingsenhetDTO?.enhet.enhetId ??
     behandlendeenhet?.geografiskEnhet?.enhetId;
   const path = `${SYFOBEHANDLENDEENHET_ROOT}/tilordningsenheter/${enhetId}`;
-  const getVeilederBrukerKnytning = () => get<Enhet[]>(path);
+  const fetchMuligeOppfolgingsenheter = () => get<Enhet[]>(path, personident);
 
   return useQuery({
-    queryKey: muligeOppfolgingsenhetQueryKeys.muligeOppfolgingsenheter(enhetId),
-    queryFn: getVeilederBrukerKnytning,
-    enabled: !!enhetId,
+    queryKey: muligeOppfolgingsenhetQueryKeys.muligeOppfolgingsenheter(
+      enhetId,
+      personident
+    ),
+    queryFn: fetchMuligeOppfolgingsenheter,
+    enabled: !!personident && !!enhetId,
   });
 }
 
