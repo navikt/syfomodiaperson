2441630570cb3edd5295b05276335564990d498b
Ingrid Gladheim <ingridgladheim@hotmail.com>
2025-11-05 13:38:40 +0100

    IS-3534: Fjerne opplysninger accordion sykmeldingssiden
    # Conflicts:
    #	src/sider/sykmeldinger/sykmelding/DinAvbrutteSykmelding.tsx
    #	src/sider/sykmeldinger/sykmelding/DinBekreftedeSykmelding.tsx
    #	src/sider/sykmeldinger/sykmelding/DinUtgaatteSykmelding.tsx

--- src/sider/sykmeldinger/sykmelding/DinAvbrutteSykmelding.tsx
+++ src/sider/sykmeldinger/sykmelding/DinAvbrutteSykmelding.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import SykmeldingStatuspanel from "../sykmeldingstatuspanel/SykmeldingStatuspanel";
 import { SykmeldingOldFormat } from "@/data/sykmelding/types/SykmeldingOldFormat";
-import { SpeilingEkspanderbartPanelTittel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanelTittel";
-import { SpeilingEkspanderbartPanel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanel";
 import { SykmeldingOpplysninger } from "@/sider/sykmeldinger/sykmelding/sykmeldingOpplysninger/SykmeldingOpplysninger";
-
-const texts = {
-  dineOpplysninger: "Opplysninger",
-};
 
 interface DinAvbrutteSykmeldingProps {
   sykmelding: SykmeldingOldFormat;
@@ -15,20 +9,10 @@ interface DinAvbrutteSykmeldingProps {
 
 const DinAvbrutteSykmelding = ({ sykmelding }: DinAvbrutteSykmeldingProps) => {
   return (
-    <div>
+    <>
       <SykmeldingStatuspanel sykmelding={sykmelding} />
-      <SpeilingEkspanderbartPanel
-        variant="lysebla"
-        defaultOpen
-        tittel={
-          <SpeilingEkspanderbartPanelTittel icon="person">
-            {texts.dineOpplysninger}
-          </SpeilingEkspanderbartPanelTittel>
-        }
-      >
-        <SykmeldingOpplysninger sykmelding={sykmelding} />
-      </SpeilingEkspanderbartPanel>
-    </div>
+      <SykmeldingOpplysninger sykmelding={sykmelding} />
+    </>
   );
 };
 
