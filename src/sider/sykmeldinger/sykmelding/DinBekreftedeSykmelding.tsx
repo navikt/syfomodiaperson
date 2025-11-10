da035d70f93e7db4907dc4547ed5465246d3843b
Ingrid Gladheim <97440960+ingring@users.noreply.github.com>
2025-11-10 10:13:30 +0100

    IS-3534: Fjerne opplysninger accordion sykmeldingssiden (#1834)

--- src/sider/sykmeldinger/sykmelding/DinBekreftedeSykmelding.tsx
+++ src/sider/sykmeldinger/sykmelding/DinBekreftedeSykmelding.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { SykmeldingOldFormat } from "@/data/sykmelding/types/SykmeldingOldFormat";
 import BekreftetSykmeldingStatuspanel from "../sykmeldingstatuspanel/BekreftetSykmeldingStatuspanel";
-import { SpeilingEkspanderbartPanel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanel";
-import { SpeilingEkspanderbartPanelTittel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanelTittel";
 import { SykmeldingOpplysninger } from "@/sider/sykmeldinger/sykmelding/sykmeldingOpplysninger/SykmeldingOpplysninger";
-
-const texts = {
-  tittel: "Opplysninger",
-};
 
 interface Props {
   sykmelding: SykmeldingOldFormat;
@@ -15,19 +9,9 @@ interface Props {
 
 export default function DinBekreftedeSykmelding({ sykmelding }: Props) {
   return (
-    <div>
+    <>
       <BekreftetSykmeldingStatuspanel sykmelding={sykmelding} />
-      <SpeilingEkspanderbartPanel
-        variant="lyselilla"
-        defaultOpen
-        tittel={
-          <SpeilingEkspanderbartPanelTittel icon="person">
-            {texts.tittel}
-          </SpeilingEkspanderbartPanelTittel>
-        }
-      >
-        <SykmeldingOpplysninger sykmelding={sykmelding} />
-      </SpeilingEkspanderbartPanel>
-    </div>
+      <SykmeldingOpplysninger sykmelding={sykmelding} />
+    </>
   );
 }
--- src/sider/sykmeldinger/sykmelding/DinUtgaatteSykmelding.tsx
+++ src/sider/sykmeldinger/sykmelding/DinUtgaatteSykmelding.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import SykmeldingStatuspanel from "../sykmeldingstatuspanel/SykmeldingStatuspanel";
 import { SykmeldingOpplysninger } from "@/sider/sykmeldinger/sykmelding/sykmeldingOpplysninger/SykmeldingOpplysninger";
 import { SykmeldingOldFormat } from "@/data/sykmelding/types/SykmeldingOldFormat";
-import { SpeilingEkspanderbartPanel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanel";
-import { SpeilingEkspanderbartPanelTittel } from "@/components/speiling/ekspanderbar/SpeilingEkspanderbartPanelTittel";
-
-const texts = {
-  tittel: "Opplysninger",
-};
 
 interface DinUtgaatteSykmeldingProps {
   sykmelding: SykmeldingOldFormat;
@@ -15,20 +9,10 @@ interface DinUtgaatteSykmeldingProps {
 
 const DinUtgatteSykmelding = ({ sykmelding }: DinUtgaatteSykmeldingProps) => {
   return (
-    <div>
+    <>
       <SykmeldingStatuspanel sykmelding={sykmelding} />
-      <SpeilingEkspanderbartPanel
-        variant="lysebla"
-        defaultOpen
-        tittel={
-          <SpeilingEkspanderbartPanelTittel icon="lege">
-            {texts.tittel}
-          </SpeilingEkspanderbartPanelTittel>
-        }
-      >
-        <SykmeldingOpplysninger sykmelding={sykmelding} />
-      </SpeilingEkspanderbartPanel>
-    </div>
+      <SykmeldingOpplysninger sykmelding={sykmelding} />
+    </>
   );
 };
 
