3ca809c281fef55fd10a11509e54fdd0d100da55
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2025-11-11 12:35:17 +0100

    IS-3536: Fjerne kode for utvidet frist (#1837)

--- src/utils/forhandsvarselUtils.ts
+++ src/utils/forhandsvarselUtils.ts
@@ -2,8 +2,6 @@ import { addWeeks } from "./datoUtils";
 
 export const getForhandsvarselFrist = (): Date => {
   const today = new Date();
-  const nov26 = new Date(today.getFullYear(), 10, 26);
-  const dec19 = new Date(today.getFullYear(), 11, 19);
-  const numberOfWeeks = nov26 < today && today < dec19 ? 6 : 3;
+  const numberOfWeeks = 3;
   return addWeeks(today, numberOfWeeks);
 };
