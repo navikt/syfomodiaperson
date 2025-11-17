d09f64b5ee5fcbc5972fc1d13b552348b11c2283
Solgård <vetle.bu.solgard@nav.no>
2025-11-17 15:54:12 +0100

    IS-3487: Add hashing of veilederident when sending event to umami

--- src/utils/umami.ts
+++ src/utils/umami.ts
@@ -1,3 +1,15 @@
-export function setIdentifier(veilederident: string) {
-  umami.identify({ veilederident });
+export async function setIdentifier(veilederident: string) {
+  const maskedVeilederIdent = await hashId(veilederident);
+  await umami.identify(maskedVeilederIdent);
+}
+
+async function hashId(id: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(id);
+
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return [...new Uint8Array(hash)]
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("")
+    .slice(0, 40);
 }
