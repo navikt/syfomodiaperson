19d536ec5dbfa3eb4de3191246fe2030a11f2006
Ingrid Gladheim <97440960+ingring@users.noreply.github.com>
2025-12-17 10:22:18 +0100

    IS-3593: Endre listevisning sykepengesøknad (#1867)

--- src/sider/sykepengsoknader/soknader/SoknaderList.tsx
+++ src/sider/sykepengsoknader/soknader/SoknaderList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SykepengesoknadDTO } from "@/data/sykepengesoknad/types/SykepengesoknadDTO";
-import { Heading } from "@navikt/ds-react";
+import { BodyShort, Box, Heading } from "@navikt/ds-react";
 import SykepengesoknadListItem from "@/sider/sykepengsoknader/soknader/SykepengesoknadListItem";
 
 interface Props {
@@ -14,20 +14,23 @@ export default function SoknaderList({
   tittel,
   tomListeTekst,
 }: Props) {
+  const hasSykepengersoknad = !!sykepengesoknader.length;
   return (
     <div className="mb-4">
-      <Heading size="small" level="2">
-        {tittel}
-      </Heading>
-      <div>
-        {sykepengesoknader.length > 0 ? (
-          sykepengesoknader.map((soknad, idx) => (
-            <SykepengesoknadListItem key={idx} soknad={soknad} />
-          ))
-        ) : (
-          <p className="panel typo-infotekst">{tomListeTekst}</p>
+      <Box background="surface-default" padding="4">
+        <Heading size="small" level="2">
+          {tittel}
+        </Heading>
+        {!hasSykepengersoknad && (
+          <BodyShort size="small" className="flex-1 mt-3">
+            {tomListeTekst}
+          </BodyShort>
         )}
-      </div>
+      </Box>
+      {hasSykepengersoknad &&
+        sykepengesoknader.map((soknad, idx) => (
+          <SykepengesoknadListItem key={idx} soknad={soknad} />
+        ))}
     </div>
   );
 }
