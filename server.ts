2b378fbda8a6f0723286b89e2b1c5585adfbc34c
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-01-26 13:53:50 +0100

    Reapply "IS: Update dependencies (#1872)" (#1890)
    This reverts commit 52ba2f1a3779714dd9e39d8225f0bcd9508554c8.

--- server.ts
+++ server.ts
@@ -2,11 +2,10 @@ import express = require("express");
 import helmet = require("helmet");
 import path = require("path");
 import prometheus = require("prom-client");
-
+import unleash = require("./server/unleash");
 import { getOpenIdClient, getOpenIdIssuer } from "./server/authUtils";
 import { setupProxy } from "./server/proxy";
 import { setupSession } from "./server/session";
-import unleash = require("./server/unleash");
 
 // Prometheus metrics
 const collectDefaultMetrics = prometheus.collectDefaultMetrics;
@@ -76,12 +75,7 @@ const setupServer = async () => {
   );
 
   server.get(
-    [
-      "/",
-      "/sykefravaer",
-      "/sykefravaer/*",
-      /^\/sykefravaer\/(?!(resources)).*$/,
-    ],
+    ["/", "/sykefravaer", /^\/sykefravaer\/(?!(resources)).*$/],
     [nocache, redirectIfUnauthorized],
     (req: express.Request, res: express.Response) => {
       res.sendFile(HTML_FILE);
