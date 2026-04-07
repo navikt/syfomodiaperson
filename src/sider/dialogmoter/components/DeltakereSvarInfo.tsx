d05ca5aedc1c2cccde0d9db96c836b39a7fab041
Peter Rydberg <peter.rydberg@bekk.no>
2026-04-07 14:53:00 +0200

    Reapply "IS-3797: Rearrange dialog panels" + fix
    This reverts commit d705f3eba618a4aa2f1e74971c3bef96e96fa704.

--- src/sider/dialogmoter/components/DeltakereSvarInfo.tsx
+++ src/sider/dialogmoter/components/DeltakereSvarInfo.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import { ArbeidsgiverSvar } from "@/sider/dialogmoter/components/svar/ArbeidsgiverSvar";
 import { ArbeidstakerSvar } from "@/sider/dialogmoter/components/svar/ArbeidstakerSvar";
 import { BehandlerSvar } from "@/sider/dialogmoter/components/svar/BehandlerSvar";
+import { Heading } from "@navikt/ds-react";
+
+const texts = {
+  deltakereSvaritle: "Svar fra deltakere",
+};
 
 interface Props {
   dialogmote: DialogmoteDTO;
@@ -10,18 +15,23 @@ interface Props {
 
 export default function DeltakereSvarInfo({ dialogmote }: Props) {
   return (
-    <div className="flex flex-col w-full gap-4">
-      <ArbeidsgiverSvar
-        varsel={dialogmote.arbeidsgiver.varselList[0]}
-        virksomhetsnummer={dialogmote.arbeidsgiver.virksomhetsnummer}
-      />
-      <ArbeidstakerSvar varsel={dialogmote.arbeidstaker.varselList[0]} />
-      {dialogmote.behandler && (
-        <BehandlerSvar
-          varsel={dialogmote.behandler.varselList[0]}
-          behandlerNavn={dialogmote.behandler.behandlerNavn}
+    <div>
+      <Heading level="3" size="small">
+        {texts.deltakereSvaritle}
+      </Heading>
+      <div className="flex flex-col w-full gap-4">
+        <ArbeidsgiverSvar
+          varsel={dialogmote.arbeidsgiver.varselList[0]}
+          virksomhetsnummer={dialogmote.arbeidsgiver.virksomhetsnummer}
         />
-      )}
+        <ArbeidstakerSvar varsel={dialogmote.arbeidstaker.varselList[0]} />
+        {dialogmote.behandler && (
+          <BehandlerSvar
+            varsel={dialogmote.behandler.varselList[0]}
+            behandlerNavn={dialogmote.behandler.behandlerNavn}
+          />
+        )}
+      </div>
     </div>
   );
 }
