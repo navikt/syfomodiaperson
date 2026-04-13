0bb36f8d6298f0213390f669654fe815c552228a
eirikdahlen <dahleneirik@gmail.com>
2026-04-13 14:33:15 +0200

    IS: Update webpack filetype
    Fikk en feil etter overgangen til esm, så vi prøver denne løsningen. Ser ut som det funker lokalt i det minste, må sjekke i dev.

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ const extensions = [".tsx", ".jsx", ".js", ".ts", ".json"];
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const commonConfig: Record<string, unknown> = {
+const commonConfig = {
   entry: {
     main: ["./src/index.tsx"],
   },
