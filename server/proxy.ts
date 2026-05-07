019346f090e329b948921b88d83c517d7b7fb4b8
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-05-07 11:31:31 +0200

    hostname fix

--- server/proxy.ts
+++ server/proxy.ts
@@ -67,7 +67,7 @@ function buildForwardedHeaders(
   if (accessToken) {
     headers["authorization"] = `Bearer ${accessToken}`;
   }
-  if (req.host === Config.auth.syfosmregister.host) {
+  if (req.hostname === Config.auth.syfosmregister.host) {
     headers["fnr"] = headers["nav-personident"];
   }
 
