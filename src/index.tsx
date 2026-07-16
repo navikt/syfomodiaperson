18941b257182fa3fd1d3867c8fc870afcb32d31c
Peter Rydberg <peter.rydberg@bekk.no>
2026-07-16 10:31:51 +0200

    IS-4148: Add Nais APM

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { minutesToMillis } from "@/utils/utils";
 import { ValgtEnhetProvider } from "@/context/ValgtEnhetContext";
 import { isClientError } from "@/api/errors";
-import { initFaro } from "@/faro";
+import "@/naisApm.ts";
 import { erLokal, erProd } from "@/utils/miljoUtil";
 
 const queryClient = new QueryClient({
@@ -32,8 +32,6 @@ const queryClient = new QueryClient({
     },
   },
 });
-
-initFaro();
 
 const container =
   document.getElementById("maincontent") || new DocumentFragment();
