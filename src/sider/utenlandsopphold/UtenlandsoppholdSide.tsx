19a1d7b384c08d2e7ce929cfe05b295c7cbf457b
Peter Rydberg <peter.rydberg@bekk.no>
2026-08-21 09:13:01 +0200

    IS-4207: Add Lumi survey to utenlandsopphold

--- src/sider/utenlandsopphold/UtenlandsoppholdSide.tsx
+++ src/sider/utenlandsopphold/UtenlandsoppholdSide.tsx
@@ -7,6 +7,9 @@ import { Menypunkter } from "@/components/globalnavigasjon/GlobalNavigasjon";
 import { NotificationProvider } from "@/context/notification/NotificationContext";
 import { UtenlandsoppholdSoknader } from "@/sider/utenlandsopphold/UtenlandsoppholdSoknader.tsx";
 import { UtenlandsoppholdSoknad } from "@/sider/utenlandsopphold/UtenlandsoppholdSoknad.tsx";
+import LumiSurvey from "@/components/lumi/LumiSurvey.tsx";
+import { utenlandsoppholdSurvey } from "@/components/lumi/utenlandsoppholdSurvey.ts";
+import { useFeatureToggles } from "@/data/unleash/unleashQueryHooks.ts";
 
 const texts = {
   title: "Søknad om sykepenger under opphold utenfor EU/EØS",
@@ -19,8 +22,25 @@ interface Props {
 }
 
 export function UtenlandsoppholdSide({ children }: Props) {
+  const { toggles } = useFeatureToggles();
+
+  const isSurveyVisible = toggles.isLumiUtenlandsoppholdEnabled;
+  const lumiSurvey = isSurveyVisible ? (
+    <LumiSurvey
+      surveyId={"modia-utenlandsopphold"}
+      survey={utenlandsoppholdSurvey}
+      behavior={{
+        dismissCooldownDays: 42,
+      }}
+    />
+  ) : null;
+
   return (
-    <Side tittel={texts.title} aktivtMenypunkt={Menypunkter.UTENLANDSOPPHOLD}>
+    <Side
+      tittel={texts.title}
+      aktivtMenypunkt={Menypunkter.UTENLANDSOPPHOLD}
+      lumi={lumiSurvey}
+    >
       <Sidetopp tittel={texts.title} />
       <SideLaster isLoading={false} isError={false}>
         <Tredelt.Container>
