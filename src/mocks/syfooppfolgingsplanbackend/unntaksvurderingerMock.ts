dda03e4f3634388f7a4f3e97fa2316d19808bfb9
Marius Havnaas <marhav95@gmail.com>
2026-09-08 15:54:58 +0200

    Fixed mocks. tests now pass.

--- src/mocks/syfooppfolgingsplanbackend/unntaksvurderingerMock.ts
+++ src/mocks/syfooppfolgingsplanbackend/unntaksvurderingerMock.ts
@@ -5,12 +5,14 @@ import {
 } from "../common/mockConstants";
 import { addWeeks } from "@/utils/datoUtils";
 
-export const unntaksvurderingerMock: UnntaksvurderingDTO[] = [
-  {
-    uuid: "3abbad77-1206-4432-b2f5-953566e5e9a1",
-    fnr: ARBEIDSTAKER_DEFAULT.personIdent,
-    organisasjonsnavn: VIRKSOMHET_PONTYPANDY.virksomhetsnavn,
-    organisasjonsnummer: VIRKSOMHET_PONTYPANDY.virksomhetsnummer,
-    meldtTidspunkt: addWeeks(new Date(), -30).toISOString(),
-  },
-];
+export const unntaksvurderingerMock: UnntaksvurderingDTO = {
+  unntaksvurderinger: [
+    {
+      uuid: "3abbad77-1206-4432-b2f5-953566e5e9a1",
+      fnr: ARBEIDSTAKER_DEFAULT.personIdent,
+      organisasjonsnavn: VIRKSOMHET_PONTYPANDY.virksomhetsnavn,
+      organisasjonsnummer: VIRKSOMHET_PONTYPANDY.virksomhetsnummer,
+      meldtTidspunkt: addWeeks(new Date(), -30).toISOString(),
+    },
+  ],
+};
--- src/sider/oppfolgingsplan/hooks/types/UnntaksvurderingDTO.ts
+++ src/sider/oppfolgingsplan/hooks/types/UnntaksvurderingDTO.ts
@@ -1,9 +1,11 @@
 export interface UnntaksvurderingDTO {
-  unntaksvurderinger: {
-    uuid: string;
-    fnr: string;
-    organisasjonsnummer: string;
-    organisasjonsnavn: string;
-    meldtTidspunkt: string;
-  }[];
+  unntaksvurderinger: [
+    {
+      uuid: string;
+      fnr: string;
+      organisasjonsnummer: string;
+      organisasjonsnavn: string;
+      meldtTidspunkt: string;
+    },
+  ];
 }
